22b9c390f6ca82f305f2b381372c86f4143ba4e2
AntoineMg <antoine.mg@outlook.com>
2021-05-06 20:13:49 +0200

    win

--- build/exe.win-amd64-3.7/ground.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-#classe sol
-class Ground(pygame.sprite.Sprite):
-
-    def __init__(self, game):
-        super().__init__()
-        self.rect = pygame.Rect(0,300,1000,150)
-        #crée un rectangle de sol !attention le 3e parametre est la largeur de la fenetre
